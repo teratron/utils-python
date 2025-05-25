d6d3bfa072c4ea6fab0bd7f848504ab47aab2c2b
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2025-05-25 12:13:36 +0300

    modified: package/__init__.py

---
+++ package/__init__.py
@@ -0,0 +1,11 @@
+__title__ = "package"
+__summary__ = "Core utilities for Python packages"
+__uri__ = "https://github.com/teratron/utils-python"
+
+__version__ = "0.1.0"
+
+__author__ = "Oleg Alexandrov"
+__email__ = "alexandrovoleg.ru@gmail.com"
+
+__license__ = "MIT or Apache-2.0"
+__copyright__ = f"2025 {__author__}"
